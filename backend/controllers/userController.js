148f9bca7dc62f0c8811700d8e53ec75b1a2aeef
Biraj Thapa <birajthapa983@gmail.com>
2024-03-21 13:16:37 +0545

    added new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,29 @@ import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
 const createUser=asyncHandler(async(req,res)=>{
-    const{userName,email,password}=req.body;
-    console.log(userName);
-    console.log(email);
-    console.log(password);
+    const{username,email,password}=req.body;
+    if (!username || !email || !password) {
+        throw new Error("Please fill all the fields");
+      }
+    const userExists = await User.findOne({ email });
+    if (userExists) res.status(400).send("User already exists");
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+       res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 })
 
 export {createUser} ;
