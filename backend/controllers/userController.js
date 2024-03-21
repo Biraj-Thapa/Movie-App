661a1dbea2a1df1481373f643f54f2886cb3ccfc
Biraj Thapa <birajthapa983@gmail.com>
2024-03-21 13:33:00 +0545

    login logic created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,8 +9,8 @@ const createUser=asyncHandler(async(req,res)=>{
         throw new Error("Please fill all the fields");
       }
     const userExists = await User.findOne({ email });
-    if (userExists) res.status(400).send("User already exists");
-    const salt = await bcrypt.genSalt(10);
+     if (userExists) res.status(400).send("User already exists");
+       const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({ username, email, password: hashedPassword });
 
@@ -29,6 +29,34 @@ const createUser=asyncHandler(async(req,res)=>{
   }
 })
 
-export {createUser} ;
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    const existingUser = await User.findOne({ email });
+  
+    if (existingUser) {
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+  
+      if (isPasswordValid) {
+        createToken(res, existingUser._id);
+  
+        res.status(201).json({
+          _id: existingUser._id,
+          username: existingUser.username,
+          email: existingUser.email,
+          isAdmin: existingUser.isAdmin,
+        });
+      } else {
+        res.status(401).json({ message:"Invalid Password"});
+      }
+    } else {
+      res.status(401).json({ message:"User not found"});
+    }
+  });
+
+export {createUser,loginUser} ;
 
     
