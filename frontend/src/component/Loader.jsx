d46ea437aebe00ef9f39646f0d7895c58b0e7fa3
Biraj Thapa <birajthapa983@gmail.com>
2024-03-23 21:48:56 +0545

    login page added

--- frontend/src/component/Loader.jsx
+++ frontend/src/component/Loader.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
+    <div className="flex justify-center items-center">
+    <div className="animate-pulse rounded-full h-16 w-16 border-4 border-teal-500 border-opacity-50">
+      <div className="h-12 w-12 border-4 border-teal-500 border-opacity-50 rounded-full"></div>
+    </div>
+  </div>
+);
 }
 
 export default Loader
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../component/Loader";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import { useLoginMutation } from "../../redux/api/users";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+  
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+  
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
+      toast.success("Logged in successfully.");
+    } catch (err) {
+      console.log(err);
+      toast.error("Invalid email or password.");
+    }
+  };
 
-export default Login
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-semibold mb-4 text-center">Login</h1>
+        <form onSubmit={submitHandler}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email Address</label>
+            <input type="email" id="email" className="mt-1 p-2 border rounded w-full" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input type="password" id="password" className="mt-1 p-2 border rounded w-full" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <button disabled={isLoading} type="submit" className="bg-teal-500 text-white px-4 py-2 rounded cursor-pointer w-full">
+            {isLoading ? "Logging in..." : "Login"}
+          </button>
+          {isLoading && <Loader />}
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-gray-600">Don't have an account? <Link to="/register" className="text-teal-500 hover:underline">Register</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
