8f5b0163816d8215cc1edf616a8ec8d96073af6c
Biraj Thapa <birajthapa983@gmail.com>
2024-03-25 21:28:58 +0545

    update genre created succesfully

--- backend/controllers/genreController.js
+++ backend/controllers/genreController.js
@@ -4,13 +4,10 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 const createGenre = asyncHandler(async (req, res) => {
     try {
       const { name } = req.body;
-  
       if (!name) {
         return res.json({ error: "Name is required" });
       }
-  
       const existingGenre = await Genre.findOne({ name });
-  
       if (existingGenre) {
         return res.json({ error: "Already exists" });
       }
@@ -22,5 +19,22 @@ const createGenre = asyncHandler(async (req, res) => {
       return res.status(400).json(error);
     }
   });
+  const updateGenre = asyncHandler(async (req, res) => {
+    try {
+      const { name } = req.body;
+      const { id } = req.params;
+      const genre = await Genre.findOne({ _id: id });
+       if (!genre) {
+        return res.status(404).json({ error: "Genre not found" });
+      }
+      genre.name = name;
+      const updatedGenre = await genre.save();
+      res.json(updatedGenre);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
   
-  export{createGenre};
+  
+  export{createGenre,updateGenre};
--- backend/routes/genreRoutes.js
+++ backend/routes/genreRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import {createGenre} from "../controllers/genreController.js";
+import {createGenre,updateGenre} from "../controllers/genreController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 router.route("/").post(authenticate, authorizeAdmin, createGenre);
+router.route("/:id").put(authenticate, authorizeAdmin, updateGenre);
 
 export default router;
