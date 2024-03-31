9e8360e4c9d12ffac19325d9ad34c4d1ed41472d
Biraj Thapa <birajthapa983@gmail.com>
2024-03-31 16:37:35 +0545

    added movie review

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -50,6 +50,7 @@ const updateMovie = async (req, res) => {
   }
 };
 
+
 const deleteMovie = async (req, res) => {
   try {
     const { id } = req.params;
@@ -65,5 +66,44 @@ const deleteMovie = async (req, res) => {
   }
 };
 
+const movieReview = async (req, res) => {
+  try {
+    const { rating, comment } = req.body;
+    const movie = await Movie.findById(req.params.id);
 
-export {createMovie,getAllMovies,getSpecificMovie,updateMovie,deleteMovie};
+    if (movie) {
+      const alreadyReviewed = movie.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+
+      if (alreadyReviewed) {
+        res.status(400);
+        throw new Error("Movie already reviewed");
+      }
+
+      const review = {
+        name: req.user.username,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+      movie.reviews.push(review);
+      movie.numReviews = movie.reviews.length;
+      movie.rating =
+        movie.reviews.reduce((acc, item) => item.rating + acc, 0) / movie.reviews.length;
+      await movie.save();
+      res.status(201).json({ message: "Review Added" });
+    } else {
+      res.status(404);
+      throw new Error("Movie not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error.message);
+  }
+};
+
+
+
+
+export {createMovie,getAllMovies,getSpecificMovie,updateMovie,deleteMovie,movieReview};
--- backend/routes/moviesRoutes.js
+++ backend/routes/moviesRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { createMovie, getAllMovies, getSpecificMovie, updateMovie, movieReview, deleteMovie,
-    deleteComment,getNewMovies,getTopMovies,getRandomMovies,} from "../controllers/movieController.js";
+import { createMovie, getAllMovies, getSpecificMovie, updateMovie, movieReview, deleteMovie}
+     from "../controllers/movieController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 router.get("/all-movies", getAllMovies);
@@ -9,4 +9,5 @@ router.get("/specific-movie/:id", getSpecificMovie);;
 router.post("/create-movie", authenticate, authorizeAdmin, createMovie);
 router.put("/update-movie/:id", authenticate, authorizeAdmin, updateMovie);
 router.delete("/delete-movie/:id", authenticate, authorizeAdmin, deleteMovie);
+router.post("/:id/reviews", authenticate, checkId, movieReview);
 export default router;
