9d9a8df00121f2f18a009d982c80a2f3f9208224
Biraj Thapa <birajthapa983@gmail.com>
2024-04-03 22:08:37 +0545

    update Movie added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,7 @@ import AdminRoute from "./pages/Admin/AdminRoutes.jsx";
 import GenreList from "./pages/Admin/GenreList.jsx";
 import CreateMovie from "./pages/Admin/CreateMovie.jsx";
 import AdminMoviesList from "./pages/Admin/AdminMoviesList.jsx"
+import UpdateMovie from "./pages/Admin/UpdateMovie.jsx"
 
 
 
@@ -31,6 +32,7 @@ const router = createBrowserRouter(
         <Route path="/admin/movies/genre" element={<GenreList />} />
          <Route path="/admin/movies-list" element={<AdminMoviesList />} />
          <Route path="/admin/movies/create" element={<CreateMovie />} />
+         <Route path="/admin/movies/update/:id" element={<UpdateMovie />} />
         </Route>
 
         </Route>)
