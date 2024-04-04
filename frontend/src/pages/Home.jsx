cb208796f1397b54320db8ef08ada74897046d5a
Biraj Thapa <birajthapa983@gmail.com>
2024-04-04 23:57:21 +0545

    added Movies container page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from './Movies/Header.jsx';
-
+import MoviesContainerPage from './Movies/MoviesContainerPage.jsx';
 const Home = () => {
   return (<>
   <Header/>
+  <section className="mt-[10rem]">
+    <MoviesContainerPage/>
+  </section>
   </>)
 }
 
